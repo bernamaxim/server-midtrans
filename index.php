59ca94875be5e68468613bda11568e31cd978cc3
bernard <bernamaxim@gmail.com>
2021-04-14 02:26:56 +0700

    try to build production3

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 // Set your server key (Note: Server key for sandbox and production mode are different)
-$server_key = 'SB-Mid-server-acFlMZ-5fEFsYMDsK840g8Ia';
-//$server_key = 'Mid-server-NyFtdTylHkn-6h3t8gn9QhHw';
+
 // Set true for production, set false for sandbox
-$is_production = false;
+$is_production = true;
 
 $api_url = $is_production ? 
   'https://app.midtrans.com/snap/v1/transactions' : 
   'https://app.sandbox.midtrans.com/snap/v1/transactions';
+
+$server_key = $is_production ? 'Mid-server-NyFtdTylHkn-6h3t8gn9QhHw' : 'SB-Mid-server-acFlMZ-5fEFsYMDsK840g8Ia'
 
 
 // Check if request doesn't contains `/charge` in the url/path, display 404
