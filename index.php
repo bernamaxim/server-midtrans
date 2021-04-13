48fe2e7bec15f40487655d904f802e6bc388ee5d
bernard <bernamaxim@gmail.com>
2021-04-14 00:09:48 +0700

    test echo server request uri7

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $api_url = $is_production ?
   'https://app.midtrans.com/snap/v1/transactions' : 
   'https://app.sandbox.midtrans.com/snap/v1/transactions';
 
-echo strpos($_SERVER["REQUEST_URI"], '/charge')
+echo strpos($_SERVER["REQUEST_URI"], '/charge');
 
 echo $_SERVER["REQUEST_URI"]
 
